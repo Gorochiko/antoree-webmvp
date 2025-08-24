2884e0cee737120991854bcda23e929a765d2cdf
TruongVu <truongvunguyen3434@gmail.com>
2025-08-24 12:05:51 +0700

    finish

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,12 +10,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-  proxy: {
-    '/api': {
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-    },
+  build: {
+    outDir: 'dist',
   },
-}
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  }
 })
